039b29eaefbe4d5027417e206887ee44eb6e5ccf
yanard <yanard18@gmail.com>
2026-06-01 01:03:47 +0300

    fix: flake8 on ex6

--- ex6/ft_data_alchemist.py
+++ ex6/ft_data_alchemist.py
@@ -18,11 +18,11 @@ if __name__ == "__main__":
     players_cap_only = [p for p in players if p[0].isupper()]
     print(f"New list of capitalized names only: {players_cap_only}\n")
 
-    score_dict = {p:random.randint(100, 999) for p in players_cap_all}
+    score_dict = {p: random.randint(100, 999) for p in players_cap_all}
     print(f"Score dict: {score_dict}")
 
     avg = sum(score_dict.values()) / len(score_dict.keys())
     print(f"Avarage score {round(avg, 2)}")
 
-    score_over_avg = {p:s for (p,s) in score_dict.items() if s > avg}
+    score_over_avg = {p: s for (p, s) in score_dict.items() if s > avg}
     print(f"High scores: {score_over_avg}")
